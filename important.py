e96dd984dc11d20085bb26b97274594871867010
Zakee Akhtar <zakeeakhtar12@gmail.com>
2021-12-09 19:39:06 +0000

    New changes

--- important.py
+++ important.py
@@ -2,3 +2,10 @@ print('hello world')
 
 svds
 print('scs')
+
+# git reset - clears staging area
+# git stash -stash changes away so you can move branch without committing after making chaneges
+# git fetch - get all branches from the remote
+
+
+# stash 1 
